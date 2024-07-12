6c78e53c7e69f168df29a95556bf2507282f5808
ptt0313 <ptt0313@gmail.com>
2024-07-12 15:48:26 +0900

    2024.07.12 에라토스테네스의 체

---
+++ Class3th (Bubble Sort)/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Class3th__Bubble_Sort_
+{
+    internal class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+        }
+        static void Main(string[] args)
+        {
+            #region 거품 정렬
+            // 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘입니다.
+
+            int[] array = new int[] { 13, 1, 21, 25, 4 };
+            int l = array.Length;
+
+            for (int i = 0; i < l - 1; i++)
+            {
+                for (int j = 0; j < (l - i) - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            #endregion
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -2,35 +2,39 @@
 {
     internal class Program
     {
-        static bool IsPrime(int number)
+        static void Swap(ref int a, ref int b)
         {
-            for (int i = 2; i < number; i++)
-            {
-                if (number % i == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
+            int c;
+            c = a;
+            a = b;
+            b = c;
         }
-            static void Main(string[] args)
+        static void Main(string[] args)
         {
-            #region 소수
-            // 1과 자신 이외에 나누어 떨어지지 않는 수입니다.
+            #region 선택 정렬
+            // 주어진 리스트 중에 최소값을 찾아서 맨 앞에 위치한 결과를
+            // 교체하는 방식으로 정렬하는 알고리즘입니다.
+            int[] array = new int[] { 13, 1, 21, 25, 4 };
 
-            //int n = int.Parse(Console.ReadLine());
-            //Console.WriteLine(IsPrime(n));
+            for(int i = 0; i < array.Length; i++)
+            {
+                int min = array[i];
+                int select = i;
 
-            #endregion
-
-            #region 합성수
-            // 1보다 큰 자연수중에서 소수가 아닌 수로, 약수가 3개 이상인 수입니다.
-            #endregion
-
-            #region 에라토스테네스의 체
-            // 소수를 판정하는 방법으로, 자연수를 순서대로 늘어놓은 표에서
-            // 합성수를 차례대로 지워나가면서 소수의 목록을 얻어내는 방법입니다.
-
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (min > array[j])
+                    {
+                        min = array[j];
+                        select = j;
+                    }
+                }
+                Swap(ref array[i], ref array[select]);
+            }
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
             #endregion
         }
     }
