600dc3acb6a5f68a5d40c62f29fe3b60192c0a32
ptt0313 <ptt0313@gmail.com>
2024-07-24 15:45:56 +0900

    2024.07.24 백트레킹

---
+++ Class16th (Euclidean Algorithm)/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Class16th__Euclidean_Algorithm_
+{
+    internal class Program
+    {
+        static int Euclidean(int a, int b)
+        {
+            //if(Math.Max(a, b) % Math.Min(a, b) == 0)
+            //{
+            //    return Math.Min(a, b);
+            //}
+            //int x = Math.Max(a,b) % Math.Min(a,b);
+            //return Euclidean(Math.Min(a, b) , x);
+
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return Euclidean(b, a % b);
+            }
+        }
+        static int GreatestCommonDivisor(int a, int b)
+        {
+            int x = 1;
+
+            for (int i = 2; i <= Math.Min(a, b); i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    x = x * i;
+                    a = a / i;
+                    b = b / i;
+                }
+            }
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            #region 유클리드 호제법
+            // 두개의 자연수 또는 정식의 최대공약수를 구하는 알고리즘으로,
+            // 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+
+            // Console.Write(GreatestCommonDivisor(5, 15));
+
+            Console.Write(Euclidean(19332, 78696));
+            #endregion
+
+
+
+        }
+    }
+   
+}
--- Program/obj/Debug/net6.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net6.0/Program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1285e6922e6f4c7573e6339804df043913abc8c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
