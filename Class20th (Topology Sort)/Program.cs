78d86cbcf3d6075055b9e25ce63626bf26cbc45d
ptt0313 <ptt0313@gmail.com>
2024-07-29 15:36:18 +0900

    2024.07.29 유니온 파인드

---
+++ Class20th (Topology Sort)/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace Class20th__Topology_Sort_
+{
+    public class Graph
+    {
+        List<int>[] graph;
+        Queue<int> queue;
+        int[] degree;
+        int size;
+        public Graph()
+        {
+            size = 8;
+            degree = new int[size];
+            queue = new Queue<int>();
+            graph = new List<int>[size];
+            for (int i = 0; i < size; i++)
+            {
+                graph[i] = new List<int>();
+            }
+        }
+        public void Insert(int vertex, int edge)
+        {
+            graph[vertex].Add(edge);
+
+            degree[edge]++;
+        }
+        public void Show()
+        {
+            for (int i = 0; i < graph.Length; i++)
+            {
+                for (int j = 0; j < graph[i].Count; j++)
+                {
+                    Console.Write(graph[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void Sort()
+        {
+            for (int i = 1; i < size; i++)
+            {
+                if (degree[i] == 0)
+                {
+                    queue.Enqueue(i);
+                }
+            }
+
+            while (queue.Count > 0)
+            {
+                int x = queue.Dequeue();
+
+                Console.Write(x + " ");
+
+                for (int i = 0; i < graph[x].Count; i++)
+                {
+                    int start = graph[x][i];
+
+                    degree[start]--;
+
+                    if (degree[start] == 0)
+                    {
+                        queue.Enqueue(start);
+                    }
+                }
+            }
+
+
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 위상 정렬
+            // 방향 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
+            // 모든 정점을 차례대로 진행하는 알고리즘입니다.
+
+            // 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
+
+            // DAG(Directed Acyclic Graph) : 사이클이 존재하지 않는 그래프
+            // 시간 복잡도 : O(V + E)
+
+            // 위상 정렬하는 방법
+
+            // 1. 진입 차수가 0인 정점을 Queue에 삽입합니다.
+
+            // 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
+
+            // 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입합니다.
+
+            // 4. Queue가 비어있을 때까지 2번에서 ~ 3번을 반복 수행합니다.
+
+            Graph graph = new Graph();
+
+            graph.Insert(1, 2);
+            graph.Insert(1, 5);
+
+            graph.Insert(2, 3);
+            graph.Insert(3, 4);
+
+            graph.Insert(4, 7);
+
+            graph.Insert(5, 6);
+            graph.Insert(6, 4);
+
+
+            // graph.Show();
+
+            graph.Sort();
+            #endregion
+        }
+    }
+    
+}
---
+++ Class21th (Union Find)/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Class21th__Union_Find_
+{
+    public class Graph
+    {
+        int[] parent;
+        int size;
+        public Graph()
+        {
+            size = 7;
+            parent = new int[size];
+            for (int i = 1; i < size; i++)
+            {
+                parent[i] = i;
+            }
+        }
+        public int Find(int x)
+        {
+            if (x == parent[x])
+            {
+                return x;
+            }
+            else
+            {
+                return Find(parent[x]);
+            }
+        }
+        public void Union(int x, int y)
+        {
+            x = Find(x);
+            y = Find(y);
+
+            if (Same(x, y))
+            {
+                return;
+            }
+
+            if (x < y)
+            {
+                parent[y] = x;
+            }
+            else
+            {
+                parent[x] = y;
+            }
+
+        }
+        public bool Same(int x, int y)
+        {
+            return (Find(x) == Find(y));
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 유니온 파인드
+            // 여러 노드가 존재할 때 어떤 두 개의 노드를 같은 집합으로
+            // 묶은 다음 어떤 두 노드가 같은 집합에 있는 지 확인하는 알고리즘입니다.
+
+            Graph graph = new Graph();
+            graph.Union(2, 3);
+            graph.Union(1, 3);
+
+            Console.WriteLine(graph.Same(2, 3));
+
+            Console.WriteLine(graph.Same(4, 5));
+            #endregion
+        }
+    }
+    
+}
