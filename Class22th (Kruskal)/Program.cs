2cf422c9bceb49a736b6d149f9ae25bb3994663a
ptt0313 <ptt0313@gmail.com>
2024-07-30 15:40:54 +0900

    2024.07.30 최소 신장 트리

---
+++ Class22th (Kruskal)/Program.cs
@@ -0,0 +1,141 @@
+﻿namespace Class22th__Kruskal_
+{
+    public class Graph
+    {
+        public class Edge : IComparable<Edge>
+        {
+            int x;
+            int y;
+            int distance;
+
+            public Edge(int x, int y, int distance)
+            {
+                this.x = x;
+                this.y = y;
+                this.distance = distance;
+            }
+            public int X
+            {
+                get { return x; }
+            }
+            public int Y
+            {
+                get { return y; }
+            }
+            public int Distance
+            {
+                get { return distance; }
+            }
+
+            public int CompareTo(Edge? other)
+            {
+                return Distance.CompareTo(other.Distance);
+            }
+        }
+        public int size;
+        public int sum;
+        public List<Edge> graph;
+        public int[] parent;
+        public Graph()
+        {
+            size = 8;
+            graph = new List<Edge>();
+            parent = new int[size];
+            for (int i = 1; i < size; i++)
+            {
+                parent[i] = i;
+            }
+        }
+        public void Insert(int x, int y, int distance)
+        {
+            Edge edge = new Edge(x, y, distance);
+            graph.Add(edge);
+        }
+
+        public int Find(int x)
+        {
+            if (x == parent[x])
+            {
+                return x;
+            }
+            else
+            {
+                return Find(parent[x]);
+            }
+        }
+        public void Union(int x, int y)
+        {
+            x = Find(x);
+            y = Find(y);
+
+            if (Same(x, y))
+            {
+                return;
+            }
+
+            if (x < y)
+            {
+                parent[y] = x;
+            }
+            else
+            {
+                parent[x] = y;
+            }
+
+        }
+        public bool Same(int x, int y)
+        {
+            return (Find(x) == Find(y));
+        }
+        public void Kruskal()
+        {
+            graph.Sort();
+            for (int i = 0; i < graph.Count; i++)
+            {
+                if (Same(graph[i].X, graph[i].Y) == false)
+                {
+                    sum += graph[i].Distance;
+                    Union(graph[i].X, graph[i].Y);
+                }
+            }
+
+            Console.WriteLine("Graph의 최소 비용 : " + sum);
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region 신장 트리
+            // 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는 그래프로,
+            // 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
+
+            // 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
+
+            Graph graph = new Graph();
+            graph.Insert(1, 2, 73);
+            graph.Insert(1, 4, 28);
+            graph.Insert(1, 5, 17);
+            graph.Insert(1, 7, 12);
+
+            graph.Insert(2, 4, 24);
+            graph.Insert(2, 5, 62);
+
+            graph.Insert(3, 5, 20);
+            graph.Insert(3, 6, 35);
+
+            graph.Insert(4, 7, 13);
+
+            graph.Insert(5, 6, 40);
+
+            graph.Kruskal();
+
+            // 최소 비용 신장 트리
+            // 그래프의 간선들의 가중치 합이 최소인 신장 트리 입니다.
+            #endregion
+        }
+    }
+    
+}
