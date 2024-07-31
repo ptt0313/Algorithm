2b1af04f80e5c7e8161734705beb5bb314665254
ptt0313 <ptt0313@gmail.com>
2024-07-31 14:09:18 +0900

    2024.07.31 누적합과 구간합

---
+++ Class23th (Prefix Sum)/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Class23th__Prefix_Sum_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 누적합
+            // 특정한 배열이 있을 때 해당 배열까지의 합을 의미합니다.
+
+            int[] array = { 7, 3, 4, 5, 1 };
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                array[i] = array[i - 1] + array[i];
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            #endregion
+        }
+    }
+   
+}
